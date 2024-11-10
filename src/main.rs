c26f029ab64053522545bc905f6922cd89a18a70
shandikadav <shandikadavid.ardiansyah@students.amikom.ac.id>
2024-11-10 22:54:38 +0700

    Add number and number convertion function

--- src/main.rs
+++ src/main.rs
@@ -50,3 +50,33 @@ fn explicit() {
     let age: i32 = 21;
     println!("{}", age);
 }
+
+#[test]
+fn number() {
+    let a: i32 = 10;
+    println!("{}", a);
+
+    let b: f64 = 10.5;
+    println!("{}", b);
+}
+
+#[test]
+fn number_convertion() {
+    let a: i8 = 10;
+    println!("{}", a);
+
+    // convert to i16
+    let b: i16 = a as i16;
+    println!("{}", b);
+
+    // convert to i32
+    let c: i32 = a as i32;
+    println!("{}", c);
+
+    let d: i64 = 1000000000;
+    println!("{}", d);
+
+    // test convert to i8 (would be overflow = 0)
+    let e: i8 = d as i8;
+    println!("{}", e);
+}
