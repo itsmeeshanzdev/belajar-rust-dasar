ffdad72a2f8e458b851a65a4a64b2551a24b6a00
shandikadav <shandikadavid.ardiansyah@students.amikom.ac.id>
2024-11-10 16:01:01 +0700

    Adding some test function

--- src/main.rs
+++ src/main.rs
@@ -3,3 +3,32 @@ fn main() {
 
     println!("Hello Shandika");
 }
+
+#[test]
+fn hello_test() {
+    println!("Hello Test");
+}
+
+#[test]
+fn test_variable() {
+    let name = "Shandika David Ardiansyah";
+    println!("Hello {}", name);
+}
+
+#[test]
+fn test_mutable() {
+    let mut name = "Shandika David Ardiansyah";
+    println!("Hello {}", name);
+
+    name = "Shandika David";
+    println!("Hello {}", name);
+}
+
+#[test]
+fn static_typing() {
+    let mut name = "Shandika David Ardiansyah";
+    println!("Hello {}", name);
+
+    // name = 10;
+    println!("Hello {}", name);
+}
